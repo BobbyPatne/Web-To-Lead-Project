bf7e950b433748057a8afdcc98886040cbeb4a5b
BobbyPatne <kaustubhpatne81@gmail.com>
2024-04-05 11:55:47 +0530

    added new function

--- script.js
+++ script.js
@@ -6,3 +6,6 @@ function beforesubmit() {
     /* we need to convert date to store data in salesforce org, so we get the locale from anonymous by system.debug(userInfo.getLocale()) -- en_IN 
     and replace hypen with the */
 }
+function bobby() {
+    console.log("Playboy");
+} 
