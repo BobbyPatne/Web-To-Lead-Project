9eb3a0234087eed428e398ea891b96faa16eb832
BobbyPatne <kaustubhpatne81@gmail.com>
2024-04-05 11:45:38 +0530

    Initialcommit

---
+++ script.js
@@ -0,0 +1,8 @@
+function beforesubmit() {
+    let inputdate = document.querySelector(".inputdate");
+    let outputdate = document.querySelector(".outputdate");
+    console.log(inputdate.value);
+    let formattedDate = new Date(inputdate.value).toDateString(en - IN);
+    /* we need to convert date to store data in salesforce org, so we get the locale from anonymous by system.debug(userInfo.getLocale()) -- en_IN 
+    and replace hypen with the */
+}
